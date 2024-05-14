14b01ca813ed7679de153ed06e59ac94233e43cc
Mateus Pontes <mateuspsdd@gmail.com>
2024-05-14 14:30:01 -0300

    Add `ProductsAMQPConfig.class`

---
+++ src/main/java/br/com/ecommerce/products/amqp/ProductsAMQPConfig.java
@@ -0,0 +1,59 @@
+package br.com.ecommerce.products.amqp;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProductsAMQPConfig {
+
+	@Bean
+	RabbitAdmin createRabbitAdmin(ConnectionFactory conn) {
+		return new RabbitAdmin(conn);
+	}
+	@Bean
+	ApplicationListener<ApplicationReadyEvent> inicializaRabbitadmin(RabbitAdmin admin) {
+		return event -> admin.initialize();
+	}
+	@Bean
+	Jackson2JsonMessageConverter messageConverter() {
+		return new Jackson2JsonMessageConverter();
+	}
+	@Bean
+	RabbitTemplate criaTemplate(ConnectionFactory conn, Jackson2JsonMessageConverter messageConverter){
+		RabbitTemplate template = new RabbitTemplate(conn);
+		template.setMessageConverter(messageConverter);
+		
+		return template;
+	}
+	
+	// Receiver configs
+	static class Receiver {
+		@Bean
+		DirectExchange discoverExchangePedidos() {
+			return ExchangeBuilder.directExchange("pedidos.criacao.ex").build();
+		}
+		@Bean
+		Queue filaStockProdutos() {
+			return QueueBuilder.nonDurable("produtos.stock-pedidos").build();
+		}
+		@Bean
+		Binding bindPedidos() {
+			return BindingBuilder
+					.bind(this.filaStockProdutos())
+					.to(this.discoverExchangePedidos())
+					.with("stock");
+		}
+	}
+}
