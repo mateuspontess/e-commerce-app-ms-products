8131eb819f5956651d433ddf978f6a7e195f4516
Mateus Pontes <mateuspsdd@gmail.com>
2024-06-06 09:09:01 -0300

    test: fix `ProductsApplicationTests`

--- src/test/java/br/com/ecommerce/products/ProductsApplicationTests.java
+++ src/test/java/br/com/ecommerce/products/ProductsApplicationTests.java
@@ -2,7 +2,11 @@ package br.com.ecommerce.products;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
+import br.com.ecommerce.products.config.Configs;
+
+@ContextConfiguration(classes = Configs.class)
 @SpringBootTest
 class ProductsApplicationTests {
 	@Test
