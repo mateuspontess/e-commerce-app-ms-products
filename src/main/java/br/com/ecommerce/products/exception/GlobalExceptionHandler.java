c740b6cb9c40f782dae0d78e4e8a1adddf0bf1c9
Mateus Pontes <mateuspsdd@gmail.com>
2024-06-09 08:34:48 -0300

    refactor: `GlobalExceptionHandler`

--- src/main/java/br/com/ecommerce/products/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/ecommerce/products/exception/GlobalExceptionHandler.java
@@ -1,8 +1,11 @@
 package br.com.ecommerce.products.exception;
 
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,57 +16,67 @@ import jakarta.persistence.EntityNotFoundException;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 	
-	private final String CREDENTIALS_ERROR_MESSAGE = "Bad credentials";
 	private final String METHOD_ARGUMENT_NOT_VALID_MESSAGE = "Input validation error";
 	private final String INTERNAL_SERVER_ERROR_MESSAGE = "Internal server error";
-
-	
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorMessage> handlerException(Exception ex) {
-    	ex.printStackTrace();
-        return ResponseEntity
-        		.internalServerError()
-        		.body(new ErrorMessage(
-        				HttpStatus.INTERNAL_SERVER_ERROR.value(),
-        				INTERNAL_SERVER_ERROR_MESSAGE));
-    }
+    
 
 	@ExceptionHandler(NoResourceFoundException.class)
     public ResponseEntity<?> handleError404(NoResourceFoundException ex) {
     	return ResponseEntity.notFound().build();
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorMessageWithFields> handlerMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
-    	var fields = ex.getFieldErrors().stream()
-    			.map(f -> new FieldErrorResponse(f.getField().toString(), f.getDefaultMessage()));
-    	var response = new ErrorMessageWithFields(
-    			HttpStatus.BAD_REQUEST.value(),
-    			METHOD_ARGUMENT_NOT_VALID_MESSAGE,
-    			fields);
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<ErrorMessage> handlerError404(EntityNotFoundException ex) {
+		return ResponseEntity.notFound().build();
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessageWithFields> handleError400(MethodArgumentNotValidException ex) {
+    	var fields = ex.getFieldErrors().stream().collect(Collectors.toMap(f -> f.getField().toString(), f -> f.getDefaultMessage()));
     	
     	return ResponseEntity
     			.badRequest()
-    			.body(response);
+    			.body(new ErrorMessageWithFields(
+					HttpStatus.BAD_REQUEST.value(),
+					METHOD_ARGUMENT_NOT_VALID_MESSAGE,
+					fields
+					)
+				);
     }
 
-    @ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity<ErrorMessage> handlerEntityNotFoundException(EntityNotFoundException ex) {
-		return ResponseEntity
-				.status(HttpStatus.UNAUTHORIZED)
-				.body(new ErrorMessage(
-						HttpStatus.UNAUTHORIZED.value(), 
-						CREDENTIALS_ERROR_MESSAGE));
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<ErrorMessage> handlerErro400(IllegalArgumentException ex) {
+		return ResponseEntity.badRequest().body(new ErrorMessage(HttpStatus.BAD_REQUEST.value(), ex.getMessage()));
 	}
-    
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<String> handlerHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleError400(HttpMessageNotReadableException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+	@ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public ResponseEntity<ErrorMessage> handlerErro415(HttpMediaTypeNotSupportedException ex) {
+        String unsupported = ex.getContentType() != null ? ex.getContentType().getType() + "/" + ex.getContentType().getSubtype() : "unknown";
+        String supported = ex.getSupportedMediaTypes().stream()
+                              .map(mediaType -> mediaType.getType() + "/" + mediaType.getSubtype())
+                              .collect(Collectors.joining(", "));
+        String message = String.format("Unsupported media type '%s'. Supported media types are: %s", unsupported, supported);
+
+        return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value()).body(new ErrorMessage(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value(), message));
+    }
+
+	@ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorMessage> handleError500(Exception ex) {
+    	ex.printStackTrace();
         return ResponseEntity
-        		.badRequest()
-        		.body(ex.getMessage());
+        		.internalServerError()
+        		.body(new ErrorMessage(
+					HttpStatus.INTERNAL_SERVER_ERROR.value(),
+					INTERNAL_SERVER_ERROR_MESSAGE
+					)
+				);
     }
 
 	private record ErrorMessage(int status, Object error) {}
 	private record ErrorMessageWithFields(int status, String error, Object fields) {}
-	private record FieldErrorResponse(String field, String message) {}
 }
