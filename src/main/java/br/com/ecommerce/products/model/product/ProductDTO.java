8fa54c3ffff2477ae2c53011fa6c1654f44711eb
Mateus Pontes <mateuspsdd@gmail.com>
2024-06-14 19:33:48 -0300

    refactor: `ProductDTO`
    - add missing annotations

--- src/main/java/br/com/ecommerce/products/model/product/ProductDTO.java
+++ src/main/java/br/com/ecommerce/products/model/product/ProductDTO.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.ecommerce.products.model.manufacturer.ManufacturerDTO;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -27,9 +28,12 @@ public class ProductDTO {
 	@NotNull
 	private Category category;
 	@NotNull
+	@Valid
     private StockDTO stock; // cria junto com o objeto Product
     @NotNull
+	@Valid
     private ManufacturerDTO manufacturer; // espera-se que já esteja criado no banco de dados
     @NotNull
+	@Valid
     private List<ProductSpecDTO> specs = new ArrayList<>(); // cria junto com o objeto
 }
