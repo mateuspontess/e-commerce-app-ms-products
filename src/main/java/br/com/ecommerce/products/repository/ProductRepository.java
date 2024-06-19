91f5c5fee48a236c122ad2269f80fa525e4e577b
Mateus Pontes <mateuspsdd@gmail.com>
2024-06-19 15:40:26 -0300

    refactor: `ProductRepository`

--- src/main/java/br/com/ecommerce/products/repository/ProductRepository.java
+++ src/main/java/br/com/ecommerce/products/repository/ProductRepository.java
@@ -16,13 +16,13 @@ import br.com.ecommerce.products.model.product.Product;
 public interface ProductRepository extends JpaRepository<Product, Long>{
 	
     @Query("""
-    		SELECT p FROM Product p WHERE
-    		(:name IS NULL OR LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%')))
-    		AND (:category IS NULL OR p.category = :category)
-    		AND (:minPrice IS NULL OR p.price >= :minPrice)
-    		AND (:maxPrice IS NULL OR p.price <= :maxPrice)
-    		AND (:manufacturerName IS NULL OR LOWER(p.manufacturer.name) = LOWER(:manufacturerName))
-    		""")
+        SELECT p FROM Product p WHERE
+        (:name IS NULL OR LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%')))
+        AND (:category IS NULL OR p.category = :category)
+        AND (:minPrice IS NULL OR p.price >= :minPrice)
+        AND (:maxPrice IS NULL OR p.price <= :maxPrice)
+        AND (:manufacturerName IS NULL OR LOWER(p.manufacturer.name) = LOWER(:manufacturerName))
+        """)
     Page<Product> findAllByParams(
             Pageable pageable,
             String name,
@@ -33,11 +33,11 @@ public interface ProductRepository extends JpaRepository<Product, Long>{
     );
     
     @Query("""
-    		SELECT DISTINCT p FROM Product p WHERE p.id 
-    		IN 
-    		(SELECT DISTINCT ps.product.id FROM ProductSpec ps 
-            WHERE 
-            (ps.attribute = :#{#specs[0][attribute]} AND ps.value = :#{#specs[0][value]}))
-            """)
+        SELECT DISTINCT p FROM Product p WHERE p.id 
+        IN 
+        (SELECT DISTINCT ps.product.id FROM ProductSpec ps 
+        WHERE 
+        (ps.attribute = :#{#specs[0][attribute]} AND ps.value = :#{#specs[0][value]}))
+        """)
      Page<Product> findProductsBySpecs(Pageable pageable, @Param("specs") List<Map<String, String>> specs);
 }
