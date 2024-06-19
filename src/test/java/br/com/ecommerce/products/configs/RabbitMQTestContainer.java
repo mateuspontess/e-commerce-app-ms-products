0f409c4e442d1a7c1857b22cf4d086df8fa7e0bc
Mateus Pontes <mateuspsdd@gmail.com>
2024-06-19 15:40:49 -0300

    test: refactor: identation correction

--- src/test/java/br/com/ecommerce/products/configs/RabbitMQTestContainer.java
+++ src/test/java/br/com/ecommerce/products/configs/RabbitMQTestContainer.java
@@ -13,11 +13,11 @@ public class RabbitMQTestContainer implements BeforeAllCallback {
     @Override
     public void beforeAll(ExtensionContext context) throws Exception {
 
-            rabbit = new RabbitMQContainer("rabbitmq:3.7.25-management-alpine")
-                .withExposedPorts(5672, 15672);
-            rabbit.start();
+        rabbit = new RabbitMQContainer("rabbitmq:3.7.25-management-alpine")
+            .withExposedPorts(5672, 15672);
+        rabbit.start();
 
-            Integer mappedPort = rabbit.getMappedPort(5672);
-            System.setProperty("spring.rabbitmq.port", mappedPort.toString());
+        Integer mappedPort = rabbit.getMappedPort(5672);
+        System.setProperty("spring.rabbitmq.port", mappedPort.toString());
     }
 }
